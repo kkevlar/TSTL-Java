662bf4da1a6c8602560667b7a9a9d40efde9cad2
flipturnapps <flipturnapps@gmail.com>
2015-06-29 15:17:22 -0700

    Partial fix; still have duplicating generated code

--- src/PropertyEntry.java
+++ src/PropertyEntry.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 
 public class PropertyEntry extends RepeatablesContainer implements RepeatablesAction
@@ -5,35 +8,69 @@ public class PropertyEntry extends RepeatablesContainer implements RepeatablesAc
 	private String[] javaCodeSplit;
 	private Repeatable[] repeatables;
 	private String myCheck;
-	
+	private HashMap<Repeatable, int[]> repeatingPoolValues;
+
 	public static String generateCheck(PropertyEntry[] propEntries)
 	{
+		ArrayList<String> pastChecks = new ArrayList<String>();
 		String ret = "public String check()\n{\n";
-				ret += "String fail = null;\n";
+		ret += "String fail = null;\n";
 		for (int i = 0; i < propEntries.length; i++) 
 		{
-			ret += propEntries[i].getMyCheck();
+			String check =  propEntries[i].getMyCheck();
+			if(!pastChecks.contains(check))
+			{
+				ret +=check;
+				pastChecks.add(check);
+			}
 		}
 		ret += "return fail;\n";
 		ret += "}\n";
 		return ret;		 
 	}
-	
+
 	public PropertyEntry(LineParsePacket packet)
 	{
 		super();
-		
+
 		this.setJavaCodeSplit(packet.getJavaCodePieces());
 		this.setRepeatables(packet.getRepeatables());
 		this.buildCheckString();
 		this.actOnValidCombinations(this);
 	}
-	
-	
+
+
 
 	private void buildCheckString() 
 	{
 		myCheck = "";
+
+		repeatingPoolValues = new HashMap<Repeatable,int[]>();
+		for(int x = 0; x < this.getRepeatables().length; x++)
+		{
+			Repeatable rep = this.getRepeatables()[x];
+			Repeatable[] reps = repeatingPoolValues.keySet().toArray(new Repeatable[repeatingPoolValues.size()]);
+			Repeatable storeRep = rep;
+			int[] newInts = new int[]{x};
+			for(int y = 0; y < reps.length; y++)
+			{
+				Repeatable testRep = reps[y];
+				boolean equal = rep.equalsRepeatable(testRep);
+				if(!equal)
+					continue;
+				int[] ints  = repeatingPoolValues.get(testRep);
+				newInts = new int[ints.length + 1];
+				for (int z = 0; z < ints.length; z++) 
+				{
+					newInts[z] = ints[z];
+				}
+				newInts[newInts.length-1] = x;
+				storeRep = testRep;
+				repeatingPoolValues.remove(testRep);
+				break;
+			}
+			repeatingPoolValues.put(storeRep, newInts);
+		}
 	}
 
 	public String[] getJavaCodeSplit() 
@@ -59,13 +96,24 @@ public class PropertyEntry extends RepeatablesContainer implements RepeatablesAc
 	@Override
 	public void actOnRepValues(int[] vals, RepeatablesContainer cont)
 	{
+		HashMap<Repeatable,Integer> pairs = new HashMap<Repeatable,Integer>();
 		String add = "";
 		for(int i = 0; i < ((this.getRepeatables().length*2)+1); i++)
 		{
 			if(i % 2 == 0)
 				add += javaCodeSplit[i/2];
 			else
-				add += this.getRepeatables()[(i-1)/2].getAsJava(vals[(i-1)/2]);
+			{
+				Repeatable r = this.getRepeatables()[(i-1)/2];
+				if(pairs.containsKey(r))
+					add += r.getAsJava(pairs.get(r));
+				else
+				{
+					int num = vals[(i-1)/2];
+					add+=r.getAsJava(num);
+					pairs.put(r, num);
+				}
+			}
 		}
 		myCheck += "if(!(" + add + "))\n";
 		myCheck += "fail =\"" + add + "\";\n";
@@ -76,6 +124,6 @@ public class PropertyEntry extends RepeatablesContainer implements RepeatablesAc
 		return myCheck;
 	}
 
-	
-	
+
+
 }
