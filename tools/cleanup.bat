d56590fee07cf9a9d0bab2bb5d03eea2373c891c
flipturnapps <flipturnapps@gmail.com>
2015-07-07 15:57:32 -0700

    Finished Windows tools

---
+++ tools/cleanup.bat
@@ -0,0 +1,6 @@
+cd ..
+RD /S /Q src\res
+RD /S /Q bin
+DEL names.file
+RD /S /Q genbin
+RD /S /Q gensrc
--- tools/run_from_source.bat
+++ tools/run_from_source.bat
@@ -1,14 +1,16 @@
-for %%* in (.) do set DIRNAME=%%~nx*
-IF %DIRNAME% EQU run-scripts cd ..
-cd copyToRes
-java -jar copyToRes.jar
 cd ..
-if not exist "bin" mkdir bin
+RD /S /Q src\res
+RD /S /Q bin
+DEL names.file
+RD /S /Q genbin
+RD /S /Q gensrc
+java -jar copyToRes/copyToRes.jar
 javac -d bin -sourcepath src src/TstlParser.java
 cd bin
 java TstlParser
 cd ..
 javac -d genbin -sourcepath gensrc gensrc/TestRunner.java
 cd genbin
+rm names.file
 java TestRunner
 pause
