865e3a23a9803929767debb788ecd35134b8210f
flipturnapps <flipturnapps@gmail.com>
2015-07-21 11:59:36 -0700

    Updated tstljava.bat

--- tools/install/tstljava.bat
+++ tools/install/tstljava.bat
@@ -4,19 +4,22 @@ set TJHOME=%USERPROFILE%\AppData\Roaming\tstljava
 java -jar %TJHOME%\tstljava.jar -p %WDIR% %*
 set /p WASHELP=<%TJHOME%\washelp.yes
 if %WASHELP%==true goto HELP
-set /p CCP=<%TJHOME%\cp.cfg
+set CCP=
+if exist %TJHOME%\cp.cfg set /p CCP=<%TJHOME%\cp.cfg
 javac -cp lib/commons-cli.jar%CCP% -d genbin -sourcepath gensrc gensrc/TestRunner.java
 set /p WANTSCC=<%TJHOME%\wantscc.yes
 if %WANTSCC%==true goto GIVECC
-java -cp lib/commons-cli.jar:genbin:.%CCP% TestRunner
+java -cp lib\commons-cli.jar;genbin;.%CCP% TestRunner
 goto EXIT
 
 :GIVECC
 set /p CCCFG =<%TJHOME%\cc.cfg.yes
-java -noverify -jar lib/emma.jar -cp lib/commons-cli.jar:genbin:.%CCP% -ix %CCCFG% TestRunner
+java -noverify -jar lib/emma.jar -cp lib/commons-cli.jar;genbin;.%CCP% -ix %CCCFG% TestRunner
 exit
-:HELP
-pause
-exit
+
 :EXIT
-exit
+pause
+goto HELP
+
+:HELP
+echo done
