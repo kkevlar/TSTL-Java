62c6046f2240f6f19f98ed2e60fa26774b137e2a
flipturnapps <flipturnapps@gmail.com>
2015-07-24 12:01:13 -0700

    Finished lookup by familyid

--- src/SmartTestReducer.java
+++ src/SmartTestReducer.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class SmartTestReducer extends TestReducer
 {
+	ArrayList<FamilyDictionaryEntry> familyDictionary;
 	public SmartTestReducer(SUTInterface sut, int[] actTraceArray, Tester tester2) 
 	{
 		super(sut, actTraceArray, tester2);
@@ -14,49 +15,58 @@ public class SmartTestReducer extends TestReducer
 
 	@Override
 	public void reduceTest() 
-	{
-		int[][] families = new int[getOriginalTestIds().length][];
-		for (int x = 0; x < families.length; x++) 
-		{
-			Action action = TstlConstants.getActionById(getSut(), this.getOriginalTestIds()[x]);
-			int famId = action.actionFamilyId();
-			for (int y = 0; y < getSut().getActions().length; y++) 
-			{
-								
-			}
-		}
-		
-		ArrayList<FamilyDictionaryEntry> familyDictionary = new ArrayList<FamilyDictionaryEntry>();
+	{		
+		familyDictionary = new ArrayList<FamilyDictionaryEntry>();
 		for (int x = 0; x < this.getSut().getActions().length; x++) 
 		{
+			boolean set = false;
 			for (int y = 0; y < familyDictionary.size(); y++) 
 			{
-				if(getSut().getActions()[x].actionFamilyId() == familyDictionary.get(y))
+				if(getSut().getActions()[x].actionFamilyId() == familyDictionary.get(y).getFamilyId())
+				{
+					familyDictionary.get(y).getActionIndices().add(x);
+					set = true;
+					break;
+				}
+			}
+			if(!set)
+			{
+				FamilyDictionaryEntry entry = new FamilyDictionaryEntry(getSut().getActions()[x].actionFamilyId());
+				entry.getActionIndices().add(x);
+				familyDictionary.add(entry);
 			}
 		}
 	}
 	public class FamilyDictionaryEntry
 	{
 		private int familyId;
-		private int[] actionIndices;
-		public FamilyDictionaryEntry(int familyId, int[] actionIndices)
+		private ArrayList<Integer> actionIndices;
+		public FamilyDictionaryEntry(int familyId)
 		{
 			super();
 			this.familyId = familyId;
-			this.actionIndices = actionIndices;
+			this.actionIndices = new ArrayList<Integer>();
 		}
-		public int getFamilyId() {
+		public int getFamilyId()
+		{
 			return familyId;
 		}
-		public void setFamilyId(int familyId) {
+		public void setFamilyId(int familyId)
+		{
 			this.familyId = familyId;
 		}
-		public int[] getActionIndices() {
+		public ArrayList<Integer> getActionIndices() 
+		{
 			return actionIndices;
 		}
-		public void setActionIndices(int[] actionIndices) {
-			this.actionIndices = actionIndices;
-		}
-		
+		public int[] getIndeciesAsArray()
+		{
+			int[] arr = new int[getActionIndices().size()];
+			for (int i = 0; i < arr.length; i++) 
+			{
+				arr[i] = getActionIndices().get(i);				
+			}
+			return arr;
+		}		
 	}
 }
