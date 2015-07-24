8f3cbd3b1f416ef06f474b4d650dd74159e7d2b7
flipturnapps <flipturnapps@gmail.com>
2015-07-24 10:28:00 -0700

    Added SmartTestReducer

---
+++ src/SmartTestReducer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class SmartTestReducer extends TestReducer
+{
+
+	public SmartTestReducer(SUTInterface sut, int[] actTraceArray, Tester tester2) 
+	{
+		super(sut, actTraceArray, tester2);
+	}
+
+	public SmartTestReducer(SUTInterface sut, ArrayList<Integer> actTrace, Tester tester) 
+	{
+		super(sut, actTrace, tester);
+	}
+
+	@Override
+	public void reduceTest() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
