20e07a50fc413ee4a41a18c0d6c4bb0fa6294b6b
flipturnapps <flipturnapps@gmail.com>
2015-07-27 09:50:25 -0700

    Created complicated fruit.

---
+++ src/com/github/flipturnapps/tstljava/testprojects/fruit/ComplicatedFruit.java
@@ -0,0 +1,14 @@
+package com.github.flipturnapps.tstljava.testprojects.fruit;
+
+public class ComplicatedFruit 
+{
+	public ComplicatedFruit(String fruitName, int size, java.awt.Color color)
+	{
+		
+	}
+	public void throwError()
+	{
+		java.awt.Color color = null;
+		color.getBlue();
+	}
+}
