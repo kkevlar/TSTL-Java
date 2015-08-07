b473d37c441c6d1f19569c896503daf08af434b9
flipturnapps <flipturnapps@gmail.com>
2015-08-07 14:31:31 -0700

    Updated master scripts to use the lang dependency

--- tools/install/tstljava.sh
+++ tools/install/tstljava.sh
@@ -5,10 +5,10 @@ exit
 fi
 CCP=$(cat $HOME/.tstljava/cp.cfg)
 if [ $(cat $HOME/.tstljava/skipcompile.yes) = false ] ; then
-javac -cp lib/commons-cli.jar$CCP -d genbin -sourcepath gensrc gensrc/TestRunner.java
+javac -cp lib/commons-cli.jar:lib/commons-lang.jar$CCP -d genbin -sourcepath gensrc gensrc/TestRunner.java
 fi
 if [ $(cat $HOME/.tstljava/wantscc.yes) = true ] ; then
-java -noverify -jar lib/emma.jar -cp lib/commons-cli.jar:genbin:.$CCP -ix $(cat $HOME/.tstljava/cc.cfg) TestRunner
+java -noverify -jar lib/emma.jar -cp lib/commons-cli.jar:lib/commons-lang.jar:genbin:.$CCP -ix $(cat $HOME/.tstljava/cc.cfg) TestRunner
 exit
 fi
 java -cp lib/commons-cli.jar:genbin:.$CCP TestRunner
