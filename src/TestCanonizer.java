97f488f4b5fca07ee35c9e651e3d875cc7c7d7c9
flipturnapps <flipturnapps@gmail.com>
2015-08-13 11:04:37 -0700

    Tenatively finished stage one of test canonizer

--- src/TestCanonizer.java
+++ src/TestCanonizer.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 
-public class TestCanonizer extends TestManipulator {
+public class TestCanonizer extends TestManipulator 
+{
 
 	public TestCanonizer(SUTInterface sut, int[] actTraceArray, Tester tester2) 
 	{
@@ -17,39 +18,92 @@ public class TestCanonizer extends TestManipulator {
 	@Override
 	public void manipulateTest() 
 	{
-		int largestValue = -1;
-		int idOfLargestValue = -1;
-		int indexOfActionToBeReplaced = -1;
+		int replaceValue = -1; //repValue of all the actions in the original test
+		int targetId = -1; //the id of the above repvalue
+		int indexOfActionToBeReplaced = -1; //sut.getActions() index of the action which has the above large repValue
 		for(int x = 0; x < getOriginalTestIds().length; x++)
 		{
 			Action action = getSut().getActions()[getOriginalTestIds()[x]];
 			for (int y = 0; y < action.repVals().length; y++) 
 			{
-				if(action.repVals()[y] > largestValue)
+				if(action.repVals()[y] > replaceValue)
 				{
-					largestValue = action.repVals()[y];
-					idOfLargestValue = action.repIds()[y];
+					replaceValue = action.repVals()[y];
+					targetId = action.repIds()[y];
 					indexOfActionToBeReplaced = x;
 				}
 			}
 		}
-		/*
-		boolean[][] valuesUsed = new boolean[largestId][largestValue];
-		for(int x = 0; x < getOriginalTestIds().length; x++)
+
+		boolean[] valuesInUse = new boolean[replaceValue]; //stores whether repVal of index is in use for the targetId
+		boolean[] actionNeedsReplacement = new boolean[getOriginalTestIds().length]; //stores whether action whose index is conained in the originalTestIds needs to be replaced
+		for(int x = 0 ; x < getOriginalTestIds().length; x++)
 		{
-			Action action = getSut().getActions()[getOriginalTestIds()[x]];
-			int min = Math.min(action.repIds().length,action.repVals().length);
-			for (int y = 0; y < min; y++)
+			Action currAction = getSut().getActions()[getOriginalTestIds()[x]];
+			for (int y = 0; y < currAction.repIds().length; y++) 
 			{
-				int repId = action.repIds()[y];
-				int repVal = action.repVals()[y];
-				valuesUsed[repId][repVal] = true;
+				int currId = currAction.repIds()[y];
+				int currVal = currAction.repVals()[y];
+				if(currId == targetId)
+				{
+					if(currVal == replaceValue)
+						actionNeedsReplacement[x] = true;
+					else if(currVal < valuesInUse.length)
+						valuesInUse[currVal] = true;
+				}
+
 			}
 		}
-		*/
-		int replacementValue = 0;
-		Action actionToBeReplaced = getSut().getActions()[indexOfActionToBeReplaced];
-			
-		
+		int targetValue = -1; //all occurrences of replaceValue when id is targetId will be replaced to this
+		for (int i = 0; i < valuesInUse.length; i++) 
+		{
+			if(!valuesInUse[i])
+			{
+				targetValue = i;
+				break;
+			}
+		}
+		int[] replacedActionIndices = new int[getOriginalTestIds().length];
+		for (int x = 0; x < replacedActionIndices.length; x++)
+		{
+			replacedActionIndices[x] = -1;
+		}
+		for (int s = 0; s < getSut().getActions().length; s++) 
+		{
+			Action currSutAction = getSut().getActions()[s];
+			for(int x = 0; x < getOriginalTestIds().length; x++)
+			{
+				Action currOrigAction = getSut().getActions()[getOriginalTestIds()[x]];
+				if(actionNeedsReplacement[x] && currOrigAction.familyId() == currSutAction.familyId())
+				{
+					int ySize = currSutAction.repIds().length; /*how many iterations the "y" loop will go. currSutAction.(repIds/repVals).length 
+					and origAction.(repIds/repVals).length should all be equivalent */
+					for (int y = 0; y < ySize; y++) 
+					{
+						int sutId = currSutAction.repIds()[y];
+						int sutVal = currSutAction.repVals()[y];
+						int origId = currOrigAction.repIds()[y];
+						int origVal = currOrigAction.repVals()[y];
+						if(sutId != origId)
+						{
+							TstlConstants.log("TestCanonizer is flawed!");
+						}
+						if(sutVal == targetValue && origVal == replaceValue)
+						{
+							replacedActionIndices[x] = s;
+							actionNeedsReplacement[x] = false;
+						}
+					}
+				}
+			}
+		}
+		for (int x = 0; x < replacedActionIndices.length; x++) 
+		{
+			if(replacedActionIndices[x] == -1)
+			{
+				replacedActionIndices[x] = getOriginalTestIds()[x];
+			}
+		}
+		this.setReducedTest(replacedActionIndices);		
 	}
 }
